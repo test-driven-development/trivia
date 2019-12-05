c04d5d574a09c34aee67bc05b4b0c4bad81d6af3
wpannell <wil.pannell@gmail.com>
2019-12-05 05:35:47 -0500

    find a better name

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/Canary.java
@@ -0,0 +1,13 @@
+package com.adaptionsoft.games.trivia;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Canary {
+
+  @Test
+  public void true_is_true() throws Exception {
+    assertTrue(true);
+  }
+}
